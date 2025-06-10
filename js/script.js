610da22e07241471a52a9ac1563a2fbaf6ae67cd
fezleep <virginio.9001@gmail.com>
2025-06-10 10:46:37 -0300

    Adicionando conteúdo para teste online

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('O JavaScript está funcionando no seu portfólio!');
+    alert('Olá do seu portfólio! Isso é um teste JavaScript.');
+});
